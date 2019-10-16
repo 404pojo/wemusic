a674b47fb22ea28f8c81d47bb15aa65ff872cc30
pojo404 <1282839117@qq.com>
2019-10-16 13:53:51 +0800

    sfs

--- src/pages/home/recommend/index.js
+++ src/pages/home/recommend/index.js
@@ -1,12 +1,80 @@
-import React from  'react'
+import React from 'react';
+import './Main.css';
+import Bg from "./Img/ear.png"
+import Author from "./Img/author.png"
+import Player from "./Img/播放.png"
+import Pause from "./Img/暂停.png"
+import { inject, observer } from "mobx-react"
+@inject('recommendStore')
+@observer
 
-class Recommend extends React.Component{
-    render(){
+
+class Recommend extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            player: Player,
+            pause: Pause,
+            active: 0,
+            song: []
+        }
+    }
+    componentDidMount() {
+        this.props.recommendStore.getList()
+        this.props.recommendStore.getSong()
+    }
+    handlePlay(el, e) {
+        e.target.src = (e.target.src === this.state.player ? this.state.pause : this.state.player)
+
+    }
+    render() {
+        const { fromJS } = require('immutable');
+        const { product, song } = this.props.recommendStore
+        const newSong = product.filter(function (item, index) {
+            return index < 28 && index > 21;
+        })
+        const songName = song.filter(function (item, index) {
+            return index < 7;
+        })
+        this.state.song = songName
+
         return (
-            <div>
-                recommend
+            <div className="Main">
+                <div className="Main_title1">
+                    <h2>推荐歌单</h2>
+                    <ul>
+                        {newSong.map(item => <li key={item.id}>
+                            <a href="">
+                                <img src={item.coverImgUrl} alt="" />
+                                <span className="bg">
+                                    <img src={Bg} alt="" />
+                                    {item.subscribedCount % 1000}万
+                                </span>
+                            </a>
+                            <p>{item.name}</p>
+                        </li>)}
+                    </ul>
+                </div>
+                <div className="Main_title2">
+                    <h2>推荐音乐</h2>
+                    <div className="song">
+                        <ul>
+                            {songName.map(item => <li key={item.id}>
+                                <div className="song_title">
+                                    <h3>{item.name}</h3>
+                                    <div className="author">
+                                        <img src={Author} alt="" />
+                                        <span>{fromJS(item).getIn(['artists', '0', 'name'])} - {fromJS(item).getIn(['album', 'type'])}</span>
+                                    </div>
+                                </div>
+                                <img src={this.state.player} className="player" onClick={this.handlePlay.bind(this, item.id)} />
+                            </li>)}
+                        </ul>
+                    </div>
+                </div>
+
             </div>
-        )
+        );
     }
 }
 
--- src/store/recommendStore.js
+++ src/store/recommendStore.js
@@ -1,6 +1,37 @@
 import {observable,runInAction,action} from 'mobx'
+// 初始化属性是一个非常重要的步骤，可以减少后期代码的错误
+// 发送异步请求，使用runInAction函数进行处理
+// 异步请求：1.请求发起时  2.请求结束时
 export default class RecommendStore{
-   constructor(rootStore){
+    @observable product = [{id:1,inventory:1,title:"sdsdsd",price:"12312"}]
+    @observable song = [{id:1,inventory:1,title:"sdsdsd",price:"12312"}]
+    @action.bound getList(){
+        const _this = this;
+        // 1.请求发起时
+        fetch("http://106.12.79.128:666/top/playlist")
+        .then(body=>body.json())
+        .then(res=>{
+            // 2.请求结束 修改状态
+            runInAction(function(){
+                // console.log(res);
+                _this.product = res.playlists
+            })
+        })
+    }
+    @action.bound getSong(){
+        const _this = this;
+        // 1.请求发起时
+        fetch("http://106.12.79.128:666/top/song?type=0")
+        .then(body=>body.json())
+        .then(res=>{
+            // 2.请求结束 修改状态
+            runInAction(function(){
+                // console.log(res);
+                _this.song = res.data;
+            })
+        })
+    }
+   structor(rootStore){
         this.rootStore=rootStore
     } 
 }
